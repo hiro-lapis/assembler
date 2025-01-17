5697eb34487077681f7c2bc68edb81623ce2f7b2
hiro-lapis <coolish555ddddd@gmail.com>
2025-01-18 00:45:59 +0900

    implement parser

--- go/main.go
+++ go/main.go
@@ -9,25 +9,56 @@ import (
 
 func main() {
 	// fmt.Println("please input compile target")
-	var fileName string
 	// fmt.Scan(&fileName)
-	fileName = "Add.asm"
+	fileNames := []string{
+		"./asm/Add.asm",
+	}
+	fileName := fileNames[0]
 	inputs, err := openFile(fileName)
 	if err != nil {
 		fmt.Println("file open error")
 		return
 	}
+
+	p := Parser{}
+	parsedLine := make([]string, 0)
+	isAList := make([]bool, 0)
+	idx := 0
 	for i := 0; i < len(inputs); i++ {
-		fmt.Println(inputs[i])
+		l, isA := p.Exec(inputs[i])
+		if l != "" {
+			parsedLine = append(parsedLine, l)
+			isAList = append(isAList, isA)
+			idx++
+			// if isAList[idx-1] {
+			// 	// 	parsedLine[idx-1] += "A!"
+			// 	// num, _ := strconv.ParseInt(parsedLine[idx-1][1:], 2, 0)
+			// }
+		}
 	}
 
-	createFile("main.hack", []string{"Hello", "World"})
+	// createFile("main.hack", []string{"Hello", "World"})
+	createFile("main.hack", parsedLine)
 }
 
 type Parser struct {
 }
 
 func (p *Parser) Exec(line string) (v string, isA bool) {
+	if len(line) == 0 {
+		return "", isA
+	}
+	index := strings.Index(line, "//")
+	if index != -1 {
+		v = line[:index]
+	} else {
+		v = line
+	}
+	v = strings.TrimSpace(v)
+	if len(v) == 0 {
+		return "", isA
+	}
+	isA = string(v[0]) == "@"
 	return v, isA
 }
 
