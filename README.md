63f448213571211863cd4b8bac79c3345203ce28
hiro-lapis <coolish555ddddd@gmail.com>
2025-01-18 22:57:31 +0900

    modify to struct orientational implementation

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # assembler
+
+## design
+
+Parser: reads and parses an instruction
+Code: translates each field into its corresponding binary value
+SymbolTable: handles symbols
+Main: initializez the I/O files and drives the process
--- go/main.go
+++ go/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -21,24 +22,119 @@ func main() {
 		return
 	}
 
-	p := Parser{}
+	p := Parser{instructions: inputs}
+	p.Next()
+	// s := SymbolTable{}
 	c := Code{}
 	parsedLine := make([]string, 0)
-	idx := 0
-	for i := 0; i < len(inputs); i++ {
-		l, isA := p.Exec(inputs[i])
-		if l != "" {
-			idx++
-			binaryStr := c.Exec(l, isA)
-			parsedLine = append(parsedLine, binaryStr)
+	for {
+		if !p.HasMoreLines() {
+			break
 		}
+		binaryStr := ""
+		if p.InstructionType() == A_INSTRUCTION {
+			binaryStr = c.ExecA(p.Label())
+		} else if p.InstructionType() == C_INSTRUCTION {
+			binaryStr = c.ExecC(p.Dest(), p.Comp(), p.Jump())
+		}
+		parsedLine = append(parsedLine, binaryStr)
+		p.Next()
 	}
 
-	// createFile("main.hack", []string{"Hello", "World"})
 	createFile("main.hack", parsedLine)
 }
 
+type InstructionType int
+
+const (
+	A_INSTRUCTION InstructionType = iota
+	C_INSTRUCTION
+	L_INSTRUCTION
+)
+
 type Parser struct {
+	instructions              []string
+	currentOriginalFileLineNo int
+	currentBinaryLineNo       int // refered line no, can be binary code line. This is counted with skiping line in the case of comment and label expression
+}
+
+func (i InstructionType) String() string {
+	return [...]string{"A_INSTRUCTION", "C_INSTRUCTION", "L_INSTRUCTION"}[i]
+}
+func (p *Parser) HasMoreLines() bool {
+	if p.currentOriginalFileLineNo-1 >= len(p.instructions) {
+		return false
+	}
+	for _, line := range p.instructions[p.currentOriginalFileLineNo:] {
+		if p.isBinarizable(line) {
+			return true
+		}
+	}
+	return false
+}
+func (p *Parser) Next() {
+	max := len(p.instructions)
+	if p.HasMoreLines() {
+		for {
+			if p.currentOriginalFileLineNo >= max-1 {
+				return
+			}
+			p.currentOriginalFileLineNo++
+			if p.isBinarizable(string(p.instructions[p.currentOriginalFileLineNo])) {
+				break
+			}
+		}
+		p.currentBinaryLineNo++
+	}
+}
+func (p *Parser) InstructionType() InstructionType {
+	line := p.instructions[p.currentOriginalFileLineNo]
+	// Next() always step to binarizable line so that we dont have to comment and space
+	if string(line[0]) == "@" {
+		isNum, _ := regexp.MatchString(`^[0-9]+$`, line[1:])
+		if isNum {
+			return A_INSTRUCTION
+		}
+		return L_INSTRUCTION
+	}
+	return C_INSTRUCTION
+}
+func (p *Parser) Label() string {
+	line := strings.TrimSpace(p.instructions[p.currentOriginalFileLineNo])
+	if p.InstructionType() == A_INSTRUCTION {
+		return line[1:]
+	}
+	return ""
+}
+func (p *Parser) Dest() string {
+	line := strings.TrimSpace(p.instructions[p.currentOriginalFileLineNo])
+	if p.InstructionType() == C_INSTRUCTION && strings.Contains(line, "=") {
+		return strings.Split(line, "=")[0]
+	}
+	return ""
+}
+func (p *Parser) Comp() string {
+	line := strings.TrimSpace(p.instructions[p.currentOriginalFileLineNo])
+	if p.InstructionType() == C_INSTRUCTION {
+		cj := strings.Split(line, "=")[1]
+		return strings.Split(cj, ";")[0]
+	}
+	return ""
+}
+func (p *Parser) Jump() string {
+	line := strings.TrimSpace(p.instructions[p.currentOriginalFileLineNo])
+	if p.InstructionType() == C_INSTRUCTION {
+		if strings.Contains(line, ";") {
+			return strings.Split(line, ";")[1]
+		}
+	}
+	return ""
+}
+
+func (p *Parser) isBinarizable(line string) bool {
+	// skip white space or comment, but label expression is binaraizale
+	str := strings.TrimSpace(line)
+	return str != "" && string(str[:2]) != "//"
 }
 
 func (p *Parser) Exec(line string) (v string, isA bool) {
@@ -84,6 +180,18 @@ func (c *Code) Exec(v string, isA bool) (result string) {
 	}
 	cccccc := c.computation(comp)
 	ddd := c.destination(dest)
+	jjj := c.jump(jump)
+	return OP_CODE_C + cccccc + ddd + jjj
+}
+func (c *Code) ExecA(symbol string) (result string) {
+	num, _ := strconv.Atoi(symbol)
+	binaryStr := strconv.FormatInt(int64(num), 2)
+	result = OP_CODE_A + strings.Repeat("0", 15-len(binaryStr)) + binaryStr
+	return result
+}
+func (c *Code) ExecC(dest string, comp string, jump string) (result string) {
+	ddd := c.destination(dest)
+	cccccc := c.computation(comp)
 	jjj := c.jump(jump)
 	return OP_CODE_C + cccccc + ddd + jjj
 }
@@ -193,7 +301,7 @@ func openFile(name string) ([]string, error) {
 	}
 	defer file.Close()
 
-	data := make([]byte, 1000)
+	data := make([]byte, 100000)
 	count, err := file.Read(data)
 	if err != nil {
 		fmt.Println(err)
