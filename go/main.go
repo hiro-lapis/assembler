217a134b2cca86eef0bdef2811fba0a77c73827f
hiro-lapis <coolish555ddddd@gmail.com>
2025-01-18 17:40:20 +0900

    implement binarize c instruction

--- go/main.go
+++ go/main.go
@@ -60,7 +60,7 @@ func (p *Parser) Exec(line string) (v string, isA bool) {
 }
 
 const OP_CODE_A = "0"
-const OP_CODE_C = "111"
+const OP_CODE_C = "1110"
 
 type Code struct {
 }
@@ -72,7 +72,109 @@ func (c *Code) Exec(v string, isA bool) (result string) {
 		result = OP_CODE_A + strings.Repeat("0", 15-len(binaryStr)) + binaryStr
 		return result
 	}
-	return OP_CODE_C + "accccccdddjjj"
+	codes := strings.Split(v, "=")
+	codes2 := strings.Split(codes[1], ";")
+	var jump, dest, comp string
+	dest = codes[0]
+	if codes2[0] != "" {
+		comp = codes2[0]
+	}
+	if len(codes2) > 1 && codes2[1] != "" {
+		jump = codes2[1]
+	}
+	cccccc := c.computation(comp)
+	ddd := c.destination(dest)
+	jjj := c.jump(jump)
+	return OP_CODE_C + cccccc + ddd + jjj
+}
+
+func (c *Code) computation(v string) string {
+	var result string
+	switch string(v) {
+	case "0":
+		return "101010"
+	case "1":
+		return "111111"
+	case "-1":
+		return "111010"
+	case "D":
+		return "001100"
+	case "A", "M":
+		return "110000"
+	case "!D":
+		return "0001101"
+	case "!A", "!M":
+		return "110001"
+	case "-D":
+		return "001111"
+	case "-A", "-M":
+		return "110011"
+	case "D+1":
+		return "011111"
+	case "A+1", "M+1":
+		return "110111"
+	case "D-1":
+		return "001110"
+	case "A-1", "M-1":
+		return "110010"
+	case "D+A", "D+M":
+		return "000010"
+	case "D-M", "D-A":
+		return "010011"
+	case "A-D", "M-D":
+		return "000111"
+	case "D&A", "D&M":
+		return "000000"
+	case "D|A", "D|M":
+		return "010101"
+	}
+	return result
+}
+
+func (c *Code) destination(v string) string {
+	var result string
+	switch string(v) {
+	case "": // null
+		return "000"
+	case "M":
+		return "001"
+	case "D":
+		return "010"
+	case "DM":
+		return "011"
+	case "A":
+		return "100"
+	case "AM":
+		return "101"
+	case "AD":
+		return "110"
+	case "ADM":
+		return "111"
+	}
+	return result
+}
+
+func (c *Code) jump(v string) string {
+	var result string
+	switch string(v) {
+	case "": // nul
+		return "000"
+	case "JGT":
+		return "001"
+	case "JEQ":
+		return "010"
+	case "JGE":
+		return "011"
+	case "JLT":
+		return "100"
+	case "JNE":
+		return "101"
+	case "JLE":
+		return "110"
+	case "JMP":
+		return "111"
+	}
+	return result
 }
 
 type SymbolTable struct {
