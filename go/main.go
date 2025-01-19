7b7a5b0baac359d75c856a25b73655288a9861e6
hiro-lapis <coolish555ddddd@gmail.com>
2025-01-19 17:02:53 +0900

    package symbol table

--- go/main.go
+++ go/main.go
@@ -4,12 +4,11 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"regexp"
-	"strconv"
 	"strings"
 
 	"github.com/hiro-lapis/assembler/code"
 	"github.com/hiro-lapis/assembler/parser"
+	"github.com/hiro-lapis/assembler/symboltable"
 )
 
 const MAX_FILE_SIZE = 10_485_760
@@ -30,7 +29,7 @@ func main() {
 		}
 
 		p := parser.NewParser(inputs)
-		s := NewSymbolTable()
+		s := symboltable.NewSymbolTable()
 		// register second path
 		setPath(p, s)
 		assemble(p, c, s, fileName)
@@ -38,7 +37,7 @@ func main() {
 }
 
 // Set symbol table's label path
-func setPath(p *parser.Parser, s *SymbolTable) {
+func setPath(p *parser.Parser, s *symboltable.SymbolTable) {
 	for {
 		if p.InstructionType() == parser.L_INSTRUCTION {
 			s.AddLabel(p.Label(), p.CurrentLine())
@@ -63,7 +62,7 @@ func spliceFileName(path string) string {
 	return path[slashIndex+1 : dotIndex]
 }
 
-func assemble(p *parser.Parser, c *code.Code, s *SymbolTable, fileName string) {
+func assemble(p *parser.Parser, c *code.Code, s *symboltable.SymbolTable, fileName string) {
 	parsedLine := make([]string, 0)
 	for {
 		binaryStr := ""
@@ -86,78 +85,7 @@ func assemble(p *parser.Parser, c *code.Code, s *SymbolTable, fileName string) {
 	createFile(newFileName+".hack", parsedLine)
 }
 
-func NewSymbolTable() *SymbolTable {
-	return &SymbolTable{
-		// pre-defined symbols
-		symbol: map[string]int{
-			"R0":     0,
-			"R1":     1,
-			"R2":     2,
-			"R3":     3,
-			"R4":     4,
-			"R5":     5,
-			"R6":     6,
-			"R7":     7,
-			"R8":     8,
-			"R9":     9,
-			"R10":    10,
-			"R11":    11,
-			"R12":    12,
-			"R13":    13,
-			"R14":    14,
-			"R15":    15,
-			"SCREEN": 16384,
-			"KBD":    24576,
-			"SP":     0,
-			"LCL":    1,
-			"ARG":    2,
-			"THIS":   3,
-			"THAT":   4,
-		},
-	}
-}
-
-const START_SECOND_PATH_IDX = 16
-
-type SymbolTable struct {
-	symbol          map[string]int // symbol:address
-	firstPathCount  int
-	secondPathCount int
-}
-
-// ng: number ahead, pre-defined word, (,),@,;
-func (s *SymbolTable) AddLabel(symbol string, currentLine int) (bin string) {
-	// if it is assign address, return the decimal number
-	isNum, _ := regexp.MatchString(`^[0-9]+$`, symbol[1:])
-	if isNum {
-		return symbol
-	}
-	if val, ok := s.symbol[symbol]; ok {
-		return string(val)
-	}
-	// since Hack assembler start line from 0, don't need to increment for next line reference
-	s.symbol[symbol] = currentLine - s.firstPathCount
-	s.firstPathCount++
-	return string(currentLine)
-}
-func (s *SymbolTable) GetValue(symbol string) (address string) {
-	address = symbol
-	// if it is assign address, return the decimal number
-	isNum, _ := regexp.MatchString(`^[0-9]+$`, symbol)
-	if isNum {
-		return address
-	}
-	if val, ok := s.symbol[symbol]; ok {
-		address = strconv.Itoa(val)
-		return address
-	}
-	s.symbol[symbol] = START_SECOND_PATH_IDX + s.secondPathCount
-	s.secondPathCount++
-	address = strconv.Itoa(s.symbol[symbol])
-	return address
-}
-
-// ファイルを開いて, その中身を返す
+// return file contents without formating
 func openFile(name string) ([]string, error) {
 	file, err := os.Open(name)
 	if err != nil {
