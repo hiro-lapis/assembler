dd08f9f066cfa84dabea8f8dfea39bbce94cf382
hiro-lapis <coolish555ddddd@gmail.com>
2025-01-19 16:54:23 +0900

    package Parser

--- go/main.go
+++ go/main.go
@@ -7,6 +7,8 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+
+	"github.com/hiro-lapis/assembler/parser"
 )
 
 const MAX_FILE_SIZE = 10_485_760
@@ -26,7 +28,7 @@ func main() {
 			return
 		}
 
-		p := NewParser(inputs)
+		p := parser.NewParser(inputs)
 		s := NewSymbolTable()
 		// register second path
 		setPath(p, s)
@@ -35,10 +37,10 @@ func main() {
 }
 
 // Set symbol table's label path
-func setPath(p *Parser, s *SymbolTable) {
+func setPath(p *parser.Parser, s *SymbolTable) {
 	for {
-		if p.InstructionType() == L_INSTRUCTION {
-			s.AddLabel(p.Label(), p.currentLine)
+		if p.InstructionType() == parser.L_INSTRUCTION {
+			s.AddLabel(p.Label(), p.CurrentLine())
 		}
 		if !p.HasMoreLines() {
 			break
@@ -60,14 +62,14 @@ func spliceFileName(path string) string {
 	return path[slashIndex+1 : dotIndex]
 }
 
-func assemble(p *Parser, c *Code, s *SymbolTable, fileName string) {
+func assemble(p *parser.Parser, c *Code, s *SymbolTable, fileName string) {
 	parsedLine := make([]string, 0)
 	for {
 		binaryStr := ""
-		if p.InstructionType() == A_INSTRUCTION {
+		if p.InstructionType() == parser.A_INSTRUCTION {
 			symol := s.GetValue(p.Symbol())
 			binaryStr = c.ExecA(symol)
-		} else if p.InstructionType() == C_INSTRUCTION {
+		} else if p.InstructionType() == parser.C_INSTRUCTION {
 			binaryStr = c.ExecC(p.Dest(), p.Comp(), p.Jump())
 		}
 
@@ -81,132 +83,6 @@ func assemble(p *Parser, c *Code, s *SymbolTable, fileName string) {
 	}
 	newFileName := spliceFileName(fileName)
 	createFile(newFileName+".hack", parsedLine)
-}
-
-type InstructionType int
-
-const (
-	A_INSTRUCTION InstructionType = iota
-	C_INSTRUCTION
-	L_INSTRUCTION
-)
-
-type Parser struct {
-	instructions []string
-	currentLine  int
-}
-
-func NewParser(lines []string) *Parser {
-	list := make([]string, 0)
-	for _, line := range lines {
-		v := ""
-		if len(line) == 0 {
-			continue
-		}
-		index := strings.Index(line, "//")
-		// skip comment
-		if index == 0 {
-			continue
-		} else if index != -1 {
-			v = line[:index]
-		} else {
-			v = line
-		}
-		v = strings.TrimSpace(v)
-		if len(v) == 0 {
-			continue
-		}
-		list = append(list, v)
-	}
-	return &Parser{instructions: list}
-}
-
-func (i InstructionType) String() string {
-	return [...]string{"A_INSTRUCTION", "C_INSTRUCTION", "L_INSTRUCTION"}[i]
-}
-func (p *Parser) HasMoreLines() bool {
-	maxIdx := len(p.instructions) - 1
-	return maxIdx > p.currentLine
-}
-func (p *Parser) Reset() {
-	p.currentLine = 0
-}
-func (p *Parser) Next() {
-	if p.HasMoreLines() {
-		p.currentLine++
-	}
-}
-func (p *Parser) InstructionType() InstructionType {
-	line := p.instructions[p.currentLine]
-	// Next() always step to binarizable line so that we dont have to comment and space
-	if string(line[0]) == "@" {
-		return A_INSTRUCTION
-	}
-	if string(line[0]) == "(" && string(line[len(line)-1]) == ")" {
-		return L_INSTRUCTION
-	}
-	return C_INSTRUCTION
-}
-func (p *Parser) Label() string {
-	if p.InstructionType() == L_INSTRUCTION {
-		return p.instructions[p.currentLine][1 : len(p.instructions[p.currentLine])-1]
-	}
-	return ""
-}
-func (p *Parser) Symbol() string {
-	if p.InstructionType() == A_INSTRUCTION {
-		return p.instructions[p.currentLine][1:]
-	}
-	return ""
-}
-func (p *Parser) Dest() string {
-	line := p.instructions[p.currentLine]
-	if p.InstructionType() == C_INSTRUCTION && strings.Contains(line, "=") {
-		return strings.Split(p.instructions[p.currentLine], "=")[0]
-	}
-	return ""
-}
-func (p *Parser) Comp() string {
-	if p.InstructionType() == C_INSTRUCTION {
-		cj := strings.Split(p.instructions[p.currentLine], "=")
-		if len(cj) > 1 {
-			return strings.Split(cj[1], ";")[0]
-		}
-		return strings.Split(cj[0], ";")[0]
-	}
-	return ""
-}
-func (p *Parser) Jump() string {
-	if p.InstructionType() == C_INSTRUCTION {
-		if strings.Contains(p.instructions[p.currentLine], ";") {
-			return strings.Split(p.instructions[p.currentLine], ";")[1]
-		}
-	}
-	return ""
-}
-
-func (p *Parser) isBinarizable(line string) bool {
-	// skip white space or comment, but label expression is binaraizale
-	str := strings.TrimSpace(line)
-	return str != "" && string(str[:2]) != "//"
-}
-
-func (p *Parser) Exec(line string) (v string, isA bool) {
-	if len(line) == 0 {
-		return "", isA
-	}
-	index := strings.Index(line, "//")
-	if index != -1 {
-		v = line[:index]
-	} else {
-		v = line
-	}
-	v = strings.TrimSpace(v)
-	if len(v) == 0 {
-		return "", isA
-	}
-	isA = string(v[0]) == "@"
-	return v, isA
 }
 
 const OP_CODE_A = "0"
