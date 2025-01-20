1208b83135425b0f630d1a2818573390a327d23b
hiro-lapis <coolish555ddddd@gmail.com>
2025-01-20 21:31:58 +0900

    add open file

--- rust/README.md
+++ rust/README.md
@@ -24,6 +24,14 @@ cargo new --bin newProject
 cargo init
 ```
 
+### Rust style guide
+[reference](https://doc.rust-lang.org/book/title-page.html)
+[reference ja](https://doc.rust-jp.rs/book-ja/title-page.html)
+[coding style guide](https://doc.rust-lang.org/nightly/style-guide/)
+
+
 ### recommended extention
 
 debug:[`CodeLLDB`](https://marketplace.visualstudio.com/items?itemName=vadimcn.vscode-lldb)
+format:[`rust-analyzer`](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+[format rule](https://rust-lang.github.io/rustfmt/?version=master&search=)
--- rust/main.rs
+++ rust/main.rs
@@ -1,26 +1,40 @@
-// even after you already figured it out. If you got everything working and feel
-// ready for the next exercise, enter `n` in the terminal.
-//
-// The exercise file will be reloaded when you change one of the lines below!
-// Try adding a new `println!` and check the updated output in the terminal.
-// compile rustc main.rs
-// run cargo run
+use std::fs::File;
+use std::io::{self, Read};
+use std::path::Path;
+
 fn main() {
-    println!(r#"       Welcome to...                      "#);
-    println!(r#"                 _   _ _                  "#);
-    println!(r#"  _ __ _   _ ___| |_| (_)_ __   __ _ ___  "#);
-    println!(r#" | '__| | | / __| __| | | '_ \ / _` / __| "#);
-    println!(r#" | |  | |_| \__ \ |_| | | | | | (_| \__ \ "#);
-    println!(r#" |_|   \__,_|___/\__|_|_|_| |_|\__, |___/ "#);
-    println!(r#"                               |___/      "#);
-    println!();
-    println!("This exercise compiles successfully. The remaining exercises contain a compiler");
-    println!("or logic error. The central concept behind Rustlings is to fix these errors and");
-    println!("solve the exercises. Good luck!");
-    println!();
-    println!("The file of this exercise is `exercises/00_intro/intro1.rs`. Have a look!");
-    println!("The current exercise path will be always shown under the progress bar.");
-    println!("You can click on the path to open the exercise file in your editor.");
+    // snake case(変数,関数)
+    let file_name = "./asm/Add.asm";
+    // match式 & Ok,Errがrustにおけるエラーハンドリングの基本形
+    match open_file(file_name) {
+        Ok(inputs) => {
+            for input in inputs {
+                println!("{}", input);
+            }
+        }
+        Err(e) => {
+            println!("file open error {}", e);
+        }
+    }
 }
 
+// pointer引数を取ることで関数実行による所有権の移転が起こさず関数にstruct引数を渡す
+// エラー発生しうる関数の戻り値はResult<T, E>型(golangでいう res, err みたいな感じ)
+fn open_file(name: &str) -> io::Result<Vec<String>> {
+    // fn open_file(name: &str) -> io::Result<Vec<String>> {
+    let path = Path::new(name);
+    // エラー発生しうる関数の後ろに?をつけることで、Ok時は後続処理、error時はearly return処理を簡潔に書ける
+    // https://doc.rust-jp.rs/book-ja/ch09-02-recoverable-errors-with-result.html#%E3%82%A8%E3%83%A9%E3%83%BC%E5%A7%94%E8%AD%B2%E3%81%AE%E3%82%B7%E3%83%A7%E3%83%BC%E3%83%88%E3%82%AB%E3%83%83%E3%83%88-%E6%BC%94%E7%AE%97%E5%AD%90
+    let mut file = File::open(path)?;
+    let mut data = String::new();
+    // 関数にmut付き引数で渡すと値の書き換えも実行関数内でできる
+    file.read_to_string(&mut data)?;
 
+    if data.is_empty() {
+        return Err(io::Error::new(io::ErrorKind::Other, "error: file is empty"));
+    }
+    // 戻り値は 式(文の区切りを意味する ; をつけるとエラーになる)
+    // closure引数の書き方|line|
+    // closure関数が1行の場合は {} をJS likeに省略可能
+    Ok(data.lines().map(|line| line.to_string()).collect())
+}
