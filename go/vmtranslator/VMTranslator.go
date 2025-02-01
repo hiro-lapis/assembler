500b36d4f6e29fec8bdcf980dc2e22f0f61a0848
hiro-lapis <coolish555ddddd@gmail.com>
2025-02-01 13:25:43 +0900

    add translation for SimpleFunction

--- go/vmtranslator/VMTranslator.go
+++ go/vmtranslator/VMTranslator.go
@@ -10,13 +10,20 @@ import (
 )
 
 const (
-	SP    = "@0" // 256
-	LCL   = "@1" // 300
-	ARG   = "@2" // 400
-	THIS  = "@3" // 3000
-	THAT  = "@4" // 3010
-	TMP   = "@5" // 5-12
-	STT   = "@6" // 16-255
+	SP   = "@0" // 256
+	LCL  = "@1" // 300
+	ARG  = "@2" // 400
+	THIS = "@3" // 3000
+	THAT = "@4" // 3010
+	TMP  = "@5" // 5-12
+	STT  = "@6" // 16-255
+	// SP    = "@0" // 256
+	// LCL   = "@1" // 300
+	// ARG   = "@2" // 400
+	// THIS  = "@3" // 3000
+	// THAT  = "@4" // 3010
+	// TMP   = "@5" // 5-12
+	// STT   = "@6" // 16-255
 	POINT = ""
 )
 
@@ -45,7 +52,8 @@ func main() {
 		// return
 		// TODO remove following code
 		// fileName = "./project8/ProgramFlow/BasicLoop/"
-		fileName = "./project8/ProgramFlow/FibonacciSeries/"
+		// fileName = "./project8/ProgramFlow/FibonacciSeries/"
+		fileName = "./project8/FunctionCalls/SimpleFunction"
 		// fileName = "StaticTest.vm"
 		// fileName := flag.Args()
 		// fileName := "SimpleAdd.vm"
@@ -75,8 +83,10 @@ func main() {
 			}
 			slash := ""
 			if string(fileName[len(fileName)-1]) != "/" {
-				slash = fileName + "/"
+				slash = "/"
 			}
+			fName := f.Name()
+			fmt.Println(fName)
 			if err = compile(fileName + slash + f.Name()); err != nil {
 				fmt.Println(err)
 			}
@@ -114,11 +124,20 @@ func compile(filePath string) error {
 			}
 			c.WritePushPop(C_POP, p.Arg1(), arg2)
 		case C_LABEL:
-			c.WriteLabel(p.LabelName())
+			c.WriteLabel(p.Arg1())
 		case C_IF:
-			c.WriteIf(p.LabelName())
+			c.WriteIf(p.Arg1())
 		case C_GOTO:
-			c.WriteGoto(p.LabelName())
+			c.WriteGoto(p.Arg1())
+		case C_FUNCTION:
+			arg2, err := p.Arg2()
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			c.WriteFunction(p.Arg1(), arg2)
+		case C_RETURN:
+			c.WriteReturn()
 		}
 
 		p.advance()
@@ -208,9 +227,6 @@ func (p *Parser) advance() {
 
 func (p *Parser) CommandType() CommandType {
 	cmds := strings.Split(p.commands[p.currentLine], " ")
-	if len(cmds) == 1 {
-		return C_ARITHMETIC
-	}
 	cmd := cmds[0]
 	switch string(cmd) {
 	case "push":
@@ -223,9 +239,14 @@ func (p *Parser) CommandType() CommandType {
 		return C_IF
 	case "goto":
 		return C_GOTO
+	case "function":
+		return C_FUNCTION
+	// TODO: implement call command
+	case "return":
+		return C_RETURN
+	default:
+		return C_ARITHMETIC
 	}
-	// TODO: implement other command types
-	return C_ARITHMETIC
 }
 func (p *Parser) Arg1() string {
 	if p.CommandType() == C_RETURN {
@@ -248,14 +269,6 @@ func (p *Parser) Arg2() (int, error) {
 	num, _ := strconv.Atoi(cmds[2])
 	return num, nil
 }
-func (p *Parser) LabelName() string {
-	cmd := p.CommandType()
-	fmt.Println(cmd)
-	if p.CommandType() != C_LABEL && p.CommandType() != C_IF && p.CommandType() != C_GOTO {
-		return ""
-	}
-	return strings.Split(p.commands[p.currentLine], " ")[1]
-}
 
 type CodeWriter struct {
 	vmCodes    []string
@@ -264,13 +277,13 @@ type CodeWriter struct {
 
 func NewCodeWriter() *CodeWriter {
 	initAsm := [][]string{
-		{"0", "256"},
-		{"1", "300"},
-		{"2", "400"},
-		{"3", "3000"},
-		{"4", "3010"},
-		{"5", "5"},
-		{"6", "16"},
+		// {"0", "256"},
+		// {"1", "300"},
+		// {"2", "400"},
+		// {"3", "3000"},
+		// {"4", "3010"},
+		// {"5", "5"},
+		// {"6", "16"},
 	}
 	setPointer := func(setTarget, value string) []string {
 		return []string{"@" + value, "D=A", "@" + setTarget, "M=D"}
@@ -506,6 +519,91 @@ func (c *CodeWriter) WriteGoto(arg2 string) {
 	result := make([]string, 0)
 	result = append(result, "@"+arg2)
 	result = append(result, "0;JMP") // uncoditional jump
+	c.vmCodes = append(c.vmCodes, result...)
+}
+func (c *CodeWriter) WriteFunction(arg1 string, arg2 int) {
+	c.WriteLabel(arg1)
+	result := make([]string, 0)
+	for i := 0; i < arg2; i++ { // initialize local variables
+		result = append(result, SP)
+		result = append(result, "A=M")
+		result = append(result, "M=0")
+		result = append(result, SP)
+		result = append(result, "M=M+1")
+	}
+	c.vmCodes = append(c.vmCodes, result...)
+}
+func (c *CodeWriter) WriteReturn() {
+	result := make([]string, 0)
+	result = append(result, LCL)
+	result = append(result, "D=M")
+	result = append(result, TMP)
+	result = append(result, "M=D") // store end frame on the TMP[0]
+
+	result = append(result, "@5") // refer to before 5 idx of LCL, the indice of return address
+	result = append(result, "D=D-A")
+	result = append(result, TMP)
+	result = append(result, "A=A+1")
+	result = append(result, "M=D") // store return Address on the TMP[1]
+
+	// 戻り値を *argument segment に設定
+	// WritePushPop を使わない(cmd追加順が狂う)
+	result = append(result, SP)
+	result = append(result, "A=M-1")
+	result = append(result, "D=M")
+	result = append(result, ARG)
+	result = append(result, "A=M")
+	result = append(result, "M=D") // *ARG= pop()
+	result = append(result, SP)
+	result = append(result, "M=M-1")
+
+	// restore caller state
+	result = append(result, ARG) // SP restore
+	result = append(result, "D=M+1")
+	result = append(result, SP)
+	result = append(result, "M=D") // SP=ARG+1
+
+	result = append(result, TMP)   // endFrame の参照
+	result = append(result, "D=M") // *(endFrame-1) のcaller THAT参照
+	result = append(result, "@1")
+	result = append(result, "D=D-A")
+	result = append(result, "A=D") // A=*THAT
+	result = append(result, "D=M") // D= caller THAT
+	result = append(result, THAT)  // *THAT= caller THAT
+	result = append(result, "M=D") // THAT=caller THAT
+
+	result = append(result, TMP) // end frame の参照
+	result = append(result, "D=M")
+	result = append(result, "@2")
+	result = append(result, "D=D-A")
+	result = append(result, "A=D") // A=THIS
+	result = append(result, "D=M") // D=*THIS
+	result = append(result, THIS)  // *THIS= caller THIS
+	result = append(result, "M=D") // THIS=caller THIS
+
+	result = append(result, TMP) // end frame の参照
+	result = append(result, "D=M")
+	result = append(result, "@3")
+	result = append(result, "D=D-A")
+	result = append(result, "A=D") // A=ARG
+	result = append(result, "D=M") // D=*ARG
+	result = append(result, ARG)   // *ARG= caller ARG
+	result = append(result, "M=D") // ARG=caller ARG
+
+	result = append(result, TMP) // end frame の参照
+	result = append(result, "D=M")
+	result = append(result, "@4")
+	result = append(result, "D=D-A")
+	result = append(result, "A=D") // A=LCL
+	result = append(result, "D=M") // D=*LCL
+	result = append(result, LCL)   // *LCL= caller LCL
+	result = append(result, "M=D") // LCL=caller LCL
+
+	// goto ret Addr
+	result = append(result, TMP)
+	result = append(result, "A=M")
+	result = append(result, "A=A+1")
+	result = append(result, "0;JMP") // return address を参照
 	c.vmCodes = append(c.vmCodes, result...)
 }
 
